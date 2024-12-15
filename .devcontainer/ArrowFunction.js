7bb647402a8991149c64a54cb0d401562e6a4816
Abhishek Kumar <106508280+rajabhishek5041@users.noreply.github.com>
2024-12-15 08:18:00 +0000

    ArrowFunction

---
+++ .devcontainer/ArrowFunction.js
@@ -0,0 +1,14 @@
+// Arrow Function:
+
+// () => {}     This is the simple syntex of Arrow function;
+
+const addTwoNum = (num1, num2) => {
+    return num1 + num2;
+}
+
+console.log(addTwoNum(3, 5))
+
+
+const addthreeNum = (num1, num2, num3) =>  num1+num2+num3 ;
+
+console.log(addthreeNum(4, 5, 1))
