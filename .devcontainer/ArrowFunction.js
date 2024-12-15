28136cc20f82df0e3668d5d1fbf75bb95036b9e2
Abhishek Kumar <106508280+rajabhishek5041@users.noreply.github.com>
2024-12-15 18:24:52 +0000

    Arrow Function

--- .devcontainer/ArrowFunction.js
+++ .devcontainer/ArrowFunction.js
@@ -3,7 +3,7 @@
 // () => {}     This is the simple syntex of Arrow function;
 
 const addTwoNum = (num1, num2) => {
-    return num1 + num2;
+    return num1 + num2;                           // This is explicit return;
 }
 
 console.log(addTwoNum(3, 5))
@@ -11,4 +11,8 @@ console.log(addTwoNum(3, 5))
 
 const addthreeNum = (num1, num2, num3) =>  num1+num2+num3 ;
 
-console.log(addthreeNum(4, 5, 1))
+console.log(addthreeNum(4, 5, 1))                     // This is implicit return;
+
+
+const addfourNum = (num1, num2, num3, num4) =>  (num1+num2+num3+num4) ;   // We can also write like this;
+console.log(addfourNum(4,5,6,2))
---
+++ .devcontainer/Life.js
@@ -0,0 +1,22 @@
+// Immediately Invoke Function Expressions (IIFE);
+
+// ()()       // This is a simple syntax of IIFE
+
+function chai() {
+    console.log(`DB Connected`)
+}
+chai();                          // This is not a IIFE technique;
+
+(function chaiCode() {                              // This is a named IIFE;
+    console.log(`This is IIFE technique`);         // This is IIFE technique ;
+}) ();                                               // We use this if we don't want to let this function intrupt from global pollution;
+
+
+(() => {                                                 
+    console.log(`This is also a IIFE technique`);
+}) ();
+
+
+((name) => {
+    console.log(`If you wanna pass a value ${name}`)
+}) ('Abhishek');
