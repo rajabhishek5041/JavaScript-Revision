8c06c449080c09612546afcb63393d77c140594b
Abhishek Kumar <106508280+rajabhishek5041@users.noreply.github.com>
2024-12-16 14:10:37 +0000

    ForEach In Object

--- .devcontainer/ForEach.js
+++ .devcontainer/ForEach.js
@@ -1,37 +1,51 @@
 
 
-const arr = [1, 2, 3, 4, 5, 10]
-for(const num of arr){
-    // console.log(num);
-}
+const coding = ["js", "java", "python", "cpp"]
+
+// coding.forEach(function (val) {
+//     console.log(val);
+    
+// })
 
 
-
-const greeting = "Hello Abhishek"
-for(const greet of greeting){
-    // console.log(greet);
-}
-
+// coding.forEach((item) => {
+//     console.log(item);
+    
+// })
 
 
-const map = new Map();
-map.set('IN', "India")
-map.set('USA', "United States of America")
-map.set('RU', "Russia")
-map.set('IS', "Isrile")
-
-// console.log(map)
-for (const [key, value] of map) {
-    // console.log(`${key} :- ${value}`)
-}
-
-
-
-const myObject = {
-    'game1': 'GTA V',
-    'game2': 'Free Fire'
-}
-
-// for (const [key, value] of myObject) {
-//         console.log(`${key} :- ${value}`)             We can't access any object with forof loop;
+// function printme(item){
+//     console.log(item);
+    
 // }
+
+// coding.forEach(printme)
+
+
+
+// coding.forEach((item, index, arr) => {
+//     console.log(item, index, arr);
+    
+// })
+
+
+
+const myCoding = [
+    {
+        languageName: "Javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "Java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "py"
+    }
+]
+
+myCoding.forEach((item) => {
+    console.log(`Language Name: ${item.languageName} & Language File Name: ${item.languageFileName}`);
+    
+})
